f411d453356b125d8974d8aeb7e2438d52626026
nofurtherinformation <dhalpern@uchicago.edu>
2026-03-12 08:37:10 -0500

    Cloudflare config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 // @ts-ignore
 import { visualizer } from "rollup-plugin-visualizer";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,7 @@ export default defineConfig({
         plugins: ["@emotion/babel-plugin"],
       },
     }),
-
+    cloudflare(),
     // visualizer({
     //   template: "treemap", // or sunburst
     //   open: true,
